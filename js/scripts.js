6fb8b487584ef8f6d7a9bf9381c6c4d20b4dbf18
gitachyut <linkinakd72@gmail.com>
2016-07-29 11:11:59 +0530

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,54 @@ $(document).ready(function(){
     if($( "#addfriends" ).hasClass( "active" )){
       addFriends();
     }
-  });
-
-  $("#rmac").click(function(e) {
-    var conf = confirm("Are You Sure?");
-    if (conf  == true) {
-      var $id = $("#user_id").val();
-      var $url = $("#base_url").val();
-      window.location.href = $url+"/monkey/delete/"+$id ;
-    } else {
-      return false;
+    if($("#friends").hasClass( "active" )){
+      Friends();
     }
+
+
   });
 
+  var Friends = function(){
+    var $id = $("#user_id").val();
+    var $url = $("#base_url").val();
+      $.post(
+        $url+"/AjaxController/friendslist/",
+        {
+          'id':$id
+        },
+        function( data ) {
 
+            if(data.output.length !=0){
+              var $output = `<table class="table table-striped table-hover ">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>`;
+              $.each(data.output, function (index, monkey) {
+                  $output += ` <tr id="`+monkey.id+`">
+                                <td>`+monkey.username+`</td>
+                                <td>`+monkey.email+`</td>
+                                <td>`+monkey.age+`</td>
+                                <td>
+                                <a href="#" class="btn btn-primary frndrm" onclick="frndrm(this.id);" id="`+monkey.id+`">Remove</a>
+                                <a href="#" class="btn btn-primary frndfav" onclick="addfrndfav(this.id);" id="`+monkey.id+`">Add To Favourite</a>
+                                </td>
+                              </tr>`;
+              });
+              $output +=`</tbody>
+            </table>`;
+          }else{
+            $output =`Nobody Found!`;
+          }
+          $("#friends").html($output);
+      });
+
+  };
 
 
   var addFriends = function(){
@@ -59,7 +93,20 @@ $(document).ready(function(){
           $("#addfriends").html($output);
       });
 
-  }
+  };
+
+
+
+  $("#rmac").click(function(e) {
+    var conf = confirm("Are You Sure?");
+    if (conf  == true) {
+      var $id = $("#user_id").val();
+      var $url = $("#base_url").val();
+      window.location.href = $url+"/monkey/delete/"+$id ;
+    } else {
+      return false;
+    }
+  });
 
  });
 
