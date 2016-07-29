3c1fa0dece960e1ae9b4ce14ef8f4638a429294c
gitachyut <linkinakd72@gmail.com>
2016-07-29 20:21:56 +0530

    Update and rename Logout.php to logout.php

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -2,7 +2,7 @@
 /**
  *
  */
-class Logout extends MainController
+class logout extends MainController
 {
   public function index(){
     $this->model('logout')->remove_session_token(Auth::logout());
